cc60597ff7449bb2cb9c8d2b910639727edbbd8b
hont <hont127@163.com>
2023-09-19 17:52:36 +0800

    Update Fsm.ts

--- Fsm.ts
+++ Fsm.ts
@@ -1,256 +1,184 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
 type StateEnter = (lastState: number, arg: any) => void;
 type StateUpdate = () => void;
 type StateExit = (newState: number) => void;
 type StateTransition = (arg: any) => boolean;
 
+@ccclass
 export class Fsm {
-  private mNestedLock: boolean;
-  private mNestedTransitionQueue: TransitionOperate[];
-  private mTransitionQuest: TransitionOperate | null;
-  private mStates: StateInfo[];
-  private mCurrentState: StateInfo | null;
 
-  constructor() {
-    this.mStates = [];
-    this.mNestedTransitionQueue = [];
-    this.mNestedLock = false;
-    this.mTransitionQuest = null;
-    this.mCurrentState = null;
-  }
+    nestedLock: boolean;
+    nestedTransitionQueue: { transition: TransitionInfo, transitionArg: any }[];
+    transitionQuest: { transition: TransitionInfo, transitionArg: any } | null;
+    stateList: StateInfo[];
+    currentState: StateInfo;
 
-  get CurrentStateIdentifier(): number {
-    return this.mCurrentState ? this.mCurrentState.identifier : -1;
-  }
 
-  public tryGetStateInfo(stateIdentifier:number): StateInfo {
-    if (!this.hasState(stateIdentifier)) {
-      this.addState(stateIdentifier);
-    }
-    return this.mStates.find((s) => s.identifier === stateIdentifier)!;
-  }
-
-  public tryGetTransitionInfo(stateIdentifier:number, dstStateIdentifier:number): TransitionInfo {
-    if (!this.hasState(stateIdentifier)) {
-      this.addState(stateIdentifier);
-    }
-    if (!this.hasState(dstStateIdentifier)) {
-      this.addState(dstStateIdentifier);
-    }
-    if (!this.hasTransition(stateIdentifier, dstStateIdentifier)) {
-      this.addTransition(stateIdentifier, dstStateIdentifier);
-    }
-    return this.getTransition(stateIdentifier, dstStateIdentifier)!;
-  }
-
-  public start(identifier: number): boolean {
-    const state = this.mStates.find((s) => s.identifier === identifier);
-    if (!state) return false;
-
-    this.mCurrentState = state;
-
-    state.onEnter.forEach((item) => {
-        item(-1, null);
-    });
-
-    return true;
-  }
-
-  public transition(dstStateIdentifier: number, arg: any = null, immediateUpdate = true): void {
-    const dstTransition = this.mCurrentState?.transitions.find(t => t.dstState.identifier === dstStateIdentifier);
-
-    this.transitionInternal({
-        transition: dstTransition,
-        transitionArg: arg,
-      },
-      immediateUpdate
-    );
-  }
-
-  public tick(isFrameStep: boolean): void {
-    if (this.mNestedLock) {
-      throw new Error("Does not support nested update fsm!");
+    constructor() {
+        this.stateList = [];
+        this.nestedTransitionQueue = [];
+        this.nestedLock = false;
     }
 
-    this.mNestedLock = true;
+    public getCurrentStateIdentifier(): number {
+        return this.currentState.identifier;
+    }
 
-    if (this.mTransitionQuest) {
-      const { transition, transitionArg } = this.mTransitionQuest;
+    public start(identifier: number): boolean {
+        const state = this.stateList.find(s => s.identifier === identifier);
+        if (!state) return false;
 
-      if (transition!.condition!(transitionArg)) {
+        this.currentState = state;
+        state.onEnter?.(-1, null);
+        return true;
+    }
 
-        transition!.selfState.onExit.forEach((item) => {
-            item(transition!.dstState.identifier);
-        });
+	public hasState(identifier: number): boolean {
+        return this.stateList.some(s => s.identifier === identifier);
+    }
+	
+	public configStateInfo(stateIdentifier:number): StateInfo|null {
+		if (!this.hasState(stateIdentifier))
+			this.addState(stateIdentifier);
 
-        transition!.dstState.onEnter.forEach(item=>{
-            item(transition!.selfState.identifier, transitionArg);
-        });
+		return this.stateList.find(s => s.identifier == stateIdentifier);
+    }
+	
+	// public TransitionInfo configTransitionInfo(stateIdentifier:number, dstStateIdentifier:number) {
+		// if (!this.hasState(stateIdentifier))
+			// this.addState(stateIdentifier);
 
-        this.mCurrentState = transition!.dstState;
-      }
+		// if (!this.hasState(dstStateIdentifier))
+			// this.addState(dstStateIdentifier);
 
-      this.mTransitionQuest = null;
-    } else {
-      const state = this.mCurrentState;
-      const transitionList = state?.transitions || [];
+		// if (!this.hasTransition(stateIdentifier, dstStateIdentifier))
+			// this.addTransition(stateIdentifier, dstStateIdentifier, null);
 
-      for (const transition of transitionList) {
-        if (transition.isAutoDetect) {
-          this.transitionInternal({ transition, transitionArg: null }, false);
+		// return this.getTransition(stateIdentifier, dstStateIdentifier);
+    // }
+
+    public transition(dstStateIdentifier: number, arg: any = null, immediateUpdate: boolean = true): void {
+        const dstTransition = this.currentState.transitionList.find(t => t.dstState.identifier === dstStateIdentifier);
+
+        this.transitionInternal({
+            transition: dstTransition,
+            transitionArg: arg
+        }, immediateUpdate);
+    }
+
+    public tick(isFrameStep: boolean): void {
+        if (this.nestedLock)
+            throw new Error("Does not support nested update fsm!");
+
+        this.nestedLock = true;
+
+        if (this.transitionQuest) {
+            const { transition, transitionArg } = this.transitionQuest;
+
+            if (transition.condition(transitionArg)) {
+                transition.selfState.onExit?.(transition.dstState.identifier);
+                transition.dstState.onEnter?.(transition.selfState.identifier, transitionArg);
+
+                this.currentState = transition.dstState;
+            }
+
+            this.transitionQuest = null;
         }
-      }
+        else {
+            const state = this.currentState;
+
+            for (let i = 0; i < state.transitionList.length; i++) {
+                const transition = state.transitionList[i];
+
+                if (transition.isAutoDetect) {
+                    this.transitionInternal({
+                        transition: transition,
+                        transitionArg: null
+                    }, false);
+                }
+            }
+        }
+
+        if (isFrameStep) {
+            this.currentState.onUpdate?.();
+        }
+
+        this.nestedLock = false;
+
+        while (this.nestedTransitionQueue.length > 0) {
+            const operate = this.nestedTransitionQueue.shift();
+            if (operate) {
+                this.transitionQuest = operate;
+                this.tick(false);
+            }
+        }
     }
 
-    if (isFrameStep) {
-      this.mCurrentState.onUpdate.forEach(item=>{
-            item();
+    public hasTransition(identifier: number, dstIdentifier: number): boolean {
+        const state = this.stateList.find(s => s.identifier === identifier);
+        if (state) {
+            return state.transitionList.some(t => t.dstState.identifier === dstIdentifier);
+        }
+
+        return false;
+    }
+
+    private addState(identifier: number, onEnter?: StateEnter, onUpdate?: StateUpdate, onExit?: StateExit): void {
+        this.stateList.push({
+            identifier: identifier,
+            transitionList: [],
+            onEnter: onEnter,
+            onUpdate: onUpdate,
+            onExit: onExit
         });
     }
 
-    this.mNestedLock = false;
+    private addTransition(stateIdentifier: number, dstStateIdentifier: number, condition: StateTransition): void {
+        const state = this.stateList.find(s => s.identifier === stateIdentifier);
+        const dstState = this.stateList.find(s => s.identifier === dstStateIdentifier);
 
-    while (this.mNestedTransitionQueue.length > 0) {
-      const operate = this.mNestedTransitionQueue.shift()!;
-      this.mTransitionQuest = operate;
-      this.tick(false);
-    }
-  }
-
-  private hasState(identifier: number): boolean {
-    return !!this.mStates.find((s) => s.identifier === identifier);
-  }
-
-  private hasTransition(identifier: number, dstIdentifier: number): boolean {
-    const state = this.mStates.find((s) => s.identifier === identifier);
-    if (state) {
-      return !!state.transitions.find((t) => t.dstState.identifier === dstIdentifier);
-    }
-    return false;
-  }
-
-  private addState(identifier: number, onEnter?: StateEnter[], onUpdate?: StateUpdate[], onExit?: StateExit[]): void {
-    let newStateInfo = new StateInfo();
-    newStateInfo.identifier = identifier;
-    if(onEnter)
-      newStateInfo.onEnter = onEnter;
-    if(onUpdate)
-      newStateInfo.onUpdate = onUpdate;
-    if(onExit)
-      newStateInfo.onExit = onExit;
-
-    this.mStates.push(newStateInfo);
-  }
-
-  private addTransition(stateIdentifier: number, dstStateIdentifier: number): void {
-    const selfState = this.mStates.find((s) => s.identifier === stateIdentifier)!;
-    const dstState = this.mStates.find((s) => s.identifier === dstStateIdentifier)!;
-
-    let transitionInfo = new TransitionInfo();
-    transitionInfo.selfState = selfState;
-    transitionInfo.dstState = dstState;
-    transitionInfo.condition = null;
-    transitionInfo.isAutoDetect = false;
-
-    selfState.transitions.push(transitionInfo);
-  }
-
-  private getTransition(stateIdentifier: number, dstStateIdentifier: number): TransitionInfo | null {
-    const stateInfo = this.mStates.find((s) => s.identifier === stateIdentifier);
-    if (!stateInfo) return null;
-    return stateInfo.transitions.find((t) => t.dstState.identifier === dstStateIdentifier) || null;
-  }
-
-  private transitionInternal(operate: TransitionOperate, immediateUpdate: boolean): void {
-    if (this.mNestedLock) {
-      this.mNestedTransitionQueue.push(operate);
-    } else {
-      this.mTransitionQuest = operate;
+        if (state && dstState) {
+            state.transitionList.push({
+                condition: condition,
+                selfState: state,
+                dstState: dstState,
+                isAutoDetect: false
+            });
+        }
     }
 
-    if (immediateUpdate) {
-      this.tick(false);
+    private getTransition(stateIdentifier: number, dstStateIdentifier: number): TransitionInfo | null {
+        const state = this.stateList.find(s => s.identifier === stateIdentifier);
+        if (!state) return null;
+        return state.transitionList.find(t => t.dstState.identifier === dstStateIdentifier) || null;
     }
-  }
+
+    private transitionInternal(operate: { transition: TransitionInfo, transitionArg: any }, immediateUpdate: boolean): void {
+        if (this.nestedLock) {
+            this.nestedTransitionQueue.push(operate);
+        } else {
+            this.transitionQuest = operate;
+        }
+
+        if (immediateUpdate) {
+            this.tick(false);
+        }
+    }
 }
 
-class StateInfo {
-  identifier: number;
-  transitions: TransitionInfo[];
-  onEnter?: StateEnter[];
-  onUpdate?: StateUpdate[];
-  onExit?: StateExit[];
-
-  public constructor() {
-    this.identifier = 0;
-    this.transitions = [];
-    this.onEnter = [];
-    this.onUpdate = [];
-    this.onExit = [];
-  }
-
-  public SetOnEnter(onEnter: StateEnter): StateInfo {
-    this.onEnter = [onEnter];
-    return this;
-  }
-
-  public AddOnEnter(onEnter: StateEnter): StateInfo {
-    if (!this.onEnter) {
-      this.onEnter = [];
-    }
-    this.onEnter.push(onEnter);
-    return this;
-  }
-
-  public SetOnUpdate(onUpdate: StateUpdate): StateInfo {
-    this.onUpdate = [onUpdate];
-    return this;
-  }
-
-  public AddOnUpdate(onUpdate: StateUpdate): StateInfo {
-    if (!this.onUpdate) {
-      this.onUpdate = [];
-    }
-    this.onUpdate.push(onUpdate);
-    return this;
-  }
-
-  public SetOnExit(onExit: StateExit): StateInfo {
-    this.onExit = [onExit];
-    return this;
-  }
-
-  public AddOnExit(onExit: StateExit): StateInfo {
-    if (!this.onExit) {
-      this.onExit = [];
-    }
-    this.onExit.push(onExit);
-    return this;
-  }
+export class StateInfo {
+    identifier: number;
+    transitionList: TransitionInfo[];
+    onEnter?: StateEnter;
+    onUpdate?: StateUpdate;
+    onExit?: StateExit;
 }
 
-class TransitionInfo {
+export class TransitionInfo {
     selfState: StateInfo;
     dstState: StateInfo;
-    condition?: StateTransition | null;
+    condition: StateTransition;
     isAutoDetect: boolean;
-
-
-    public constructor() {
-      this.selfState = null;
-      this.dstState = null;
-      this.condition = null;
-      this.isAutoDetect = false;
-    }
-
-    public SetTransition(condition: StateTransition, autoDetect: boolean = false): TransitionInfo {
-        this.condition = condition;
-        this.isAutoDetect = autoDetect;
-
-        return this;
-    }
-}
-
-class TransitionOperate {
-  transition: TransitionInfo | null;
-  transitionArg: any;
 }
